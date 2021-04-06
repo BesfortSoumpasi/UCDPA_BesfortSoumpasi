0145b32408661cc403a9681e560efe1d7c8e43f6
BesfortSoumpasi <bessoumpasi@gmail.com>
2021-04-06 19:10:11 +0100

    setting up

---
+++ VgaSales.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+import matplotlib as plt
+import os
+
+os.chdir(r"C:\Users\besso\OneDrive\Desktop\UCD")
+
+vga = pd.read_csv("vgsales.csv", encoding="UTF-8")
+
+print(vga.head)
