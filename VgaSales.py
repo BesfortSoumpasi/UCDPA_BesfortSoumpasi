d8fb9b257d03f8e44d314767f7d1c551a013b8b9
BesfortSoumpasi <bessoumpasi@gmail.com>
2021-04-06 19:20:04 +0100

    2. finding out shape, types and duplicate values (if the same game is added twice)

--- VgaSales.py
+++ VgaSales.py
@@ -7,4 +7,7 @@ os.chdir(r"C:\Users\besso\OneDrive\Desktop\UCD")
 
 vga = pd.read_csv("vgsales.csv", encoding="UTF-8")
 
-print(vga.head)
+print(vga.shape)
+print(vga.dtypes)
+print(len(vga.Name.unique()))
+
