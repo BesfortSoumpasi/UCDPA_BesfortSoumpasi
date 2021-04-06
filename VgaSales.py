ef35faa43906fdb893e0a74d6cbb24db697ad806
BesfortSoumpasi <bessoumpasi@gmail.com>
2021-04-06 21:09:27 +0100

    7. splitting the 5 region columns into different datasets

--- VgaSales.py
+++ VgaSales.py
@@ -11,4 +11,8 @@ vga1 = vga.drop_duplicates("Name")
 
 vga2 = vga1.dropna()
 
-print(vga2.isnull().sum())
+vga_NA = vga2[["Name", "Year", "NA_Sales"]]
+vga_EU = vga2[["Name", "Year", "EU_Sales"]]
+vga_JP = vga2[["Name", "Year", "JP_Sales"]]
+vga_Ot = vga2[["Name", "Year", "Other_Sales"]]
+vga_GL = vga2[["Name", "Year", "Global_Sales"]]
